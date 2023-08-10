40223f9bb17777e4f2e3f0e52c17a095a577f36f
cunhavcs <cunha.vcs@gmail.com>
2023-08-10 17:06:00 -0300

    req 08: cria class UserService

---
+++ app/backend/src/services/UserService.ts
@@ -0,0 +1,26 @@
+import UserModel from '../models/UserModel';
+import { IUser } from '../Interfaces/users/IUser';
+import { ServiceResponse } from '../Interfaces/ServiceResponse';
+
+export default class UserService {
+  constructor(
+    private userModel = new UserModel(),
+  ) {}
+
+  async getUsers(): Promise<ServiceResponse<IUser[]>> {
+    const users = await this.userModel.findAll();
+    return { statusCode: 200, data: users };
+  }
+
+  async getUserById(id: number): Promise<ServiceResponse<IUser>> {
+    const user = await this.userModel.findById(id);
+    if (!user) return { statusCode: 404, data: { message: 'User not found' } };
+    return { statusCode: 200, data: user };
+  }
+
+  async getUserByEmail(email: string): Promise<ServiceResponse<IUser>> {
+    const user = await this.userModel.findByEmail(email);
+    if (!user) return { statusCode: 404, data: { message: 'User not found' } };
+    return { statusCode: 200, data: user };
+  }
+}
