07c22ba56f7ddc974b6214a129ff86cb925d313b
cunhavcs <cunha.vcs@gmail.com>
2023-08-10 08:59:19 -0300

    req 03: cria a interface ITeamModel e a class TeamModel

---
+++ app/backend/src/models/TeamModel.ts
@@ -0,0 +1,12 @@
+import SequelizeTeam from '../database/models/SequelizeTeam';
+import { ITeam } from '../Interfaces/ITeam';
+import { ITeamModel } from '../Interfaces/ITeamModel';
+
+export default class TeamModel implements ITeamModel {
+  private model = SequelizeTeam;
+
+  async findAll(): Promise<ITeam[]> {
+    const teams = await this.model.findAll();
+    return teams;
+  }
+}
