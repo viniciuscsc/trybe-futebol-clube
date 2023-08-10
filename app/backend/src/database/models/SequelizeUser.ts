a84e9c3d86e85ee97389e7a1246aba845c2a475f
cunhavcs <cunha.vcs@gmail.com>
2023-08-10 16:39:00 -0300

    req 06: cria model SequelizeTeam

---
+++ app/backend/src/database/models/SequelizeUser.ts
@@ -0,0 +1,49 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+export default class SequelizeUser extends Model<InferAttributes<SequelizeUser>> {
+  declare id: CreationOptional<number>;
+
+  declare username: string;
+
+  declare role: string;
+
+  declare email: string;
+
+  declare password: string;
+}
+
+SequelizeUser.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'users',
+  underscored: true,
+  timestamps: false,
+});
