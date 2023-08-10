955f9d8a8277cb32085a1a9f56b25cab3abbaf9c
cunhavcs <cunha.vcs@gmail.com>
2023-08-10 15:47:32 -0300

    req 05: cria metodo getTeamById em class TeamController

--- app/backend/src/controllers/TeamController.ts
+++ app/backend/src/controllers/TeamController.ts
@@ -6,8 +6,14 @@ export default class TeamController {
     private teamService = new TeamService(),
   ) {}
 
-  async getTeams(req: Request, res: Response): Promise<Response> {
+  async getTeams(_req: Request, res: Response): Promise<Response> {
     const { statusCode, data } = await this.teamService.getTeams();
+    return res.status(statusCode).json(data);
+  }
+
+  async getTeamById(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const { statusCode, data } = await this.teamService.getTeamById(Number(id));
     return res.status(statusCode).json(data);
   }
 }
