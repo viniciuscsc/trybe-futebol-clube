f9d0adb648d054c78191a2147a92cf71f5850475
cunhavcs <cunha.vcs@gmail.com>
2023-08-15 19:02:53 -0300

    req 17: cria endpoint PUT em /:id/finish

--- app/backend/src/routes/match.route.ts
+++ app/backend/src/routes/match.route.ts
@@ -1,10 +1,16 @@
 import { Request, Response, Router } from 'express';
 import MatchController from '../controllers/MatchController';
+import Validations from '../middlewares/Validations';
 
 const matchController = new MatchController();
 
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => matchController.getMatches(req, res));
+router.put(
+  '/:id/finish',
+  Validations.validateToken,
+  (req: Request, res: Response) => matchController.endGame(req, res),
+);
 
 export default router;
