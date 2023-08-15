69ec3a0cc40c5d210c39bb9252bcb1eb47e7d9d3
cunhavcs <cunha.vcs@gmail.com>
2023-08-15 18:56:40 -0300

    req 17: cria o metodo endGame em MatchService

--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -1,6 +1,6 @@
 import MatchModel from '../models/MatchModel';
 
-import { ServiceResponse } from '../Interfaces/ServiceResponse';
+import { ServiceMessage, ServiceResponse } from '../Interfaces/ServiceResponse';
 import { IMatch } from '../Interfaces/matches/IMatch';
 
 export default class MatchService {
@@ -11,5 +11,10 @@ export default class MatchService {
   async getMatches(inProgress?: boolean): Promise<ServiceResponse<IMatch[]>> {
     const matches = await this.matchModel.findAll(inProgress);
     return { statusCode: 200, data: matches };
+  }
+
+  async endGame(id: number): Promise<ServiceResponse<ServiceMessage>> {
+    await this.matchModel.update(id);
+    return { statusCode: 200, data: { message: 'Finished' } };
   }
 }
