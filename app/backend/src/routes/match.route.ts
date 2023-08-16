b38b2d8bf483ccb20b2390b93c002553d90dd187
cunhavcs <cunha.vcs@gmail.com>
2023-08-16 12:25:29 -0300

    req 21: cria o endpoint POST /

--- app/backend/src/routes/match.route.ts
+++ app/backend/src/routes/match.route.ts
@@ -20,4 +20,9 @@ router.patch(
   (req: Request, res: Response) => matchController.updateMatch(req, res),
 );
 
+router.post(
+  '/',
+  (req: Request, res: Response) => matchController.createMatch(req, res),
+);
+
 export default router;
