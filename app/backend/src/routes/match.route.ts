1a194582234439b6ace28a818791d93ea8ed273f
cunhavcs <cunha.vcs@gmail.com>
2023-08-16 10:41:59 -0300

    req 18: cria endpoint PATCH em /matches/:id

--- app/backend/src/routes/match.route.ts
+++ app/backend/src/routes/match.route.ts
@@ -7,10 +7,17 @@ const matchController = new MatchController();
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => matchController.getMatches(req, res));
+
 router.patch(
   '/:id/finish',
   Validations.validateToken,
   (req: Request, res: Response) => matchController.endMatch(req, res),
+);
+
+router.patch(
+  '/:id',
+  Validations.validateToken,
+  (req: Request, res: Response) => matchController.updateMatch(req, res),
 );
 
 export default router;
