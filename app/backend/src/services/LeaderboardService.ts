3e18df33f4aefd6a09da270bdbd7fa775f4d523a
cunhavcs <cunha.vcs@gmail.com>
2023-08-18 14:10:38 -0300

    req 24: refatora sortedLeaderboard em teamStats e leaderboardHome em LeaderboardService

--- app/backend/src/services/LeaderboardService.ts
+++ app/backend/src/services/LeaderboardService.ts
@@ -4,24 +4,26 @@ import MatchModel from '../models/MatchModel';
 import { ServiceResponse } from '../Interfaces/ServiceResponse';
 import { ITeamStats } from '../Interfaces/teams/ITeamStats';
 
-import { getTeamStats } from '../utils/teamStats.util';
+import { getTeamStats, sortLeaderboard } from '../utils/teamStats.util';
 
 export default class LeaderboardService {
   constructor(
     private teamModel = new TeamModel(),
     private matchModel = new MatchModel(),
-    private _leaderboardHome: ITeamStats[] = [],
   ) {}
 
   async leaderboardHome(): Promise<ServiceResponse<ITeamStats[]>> {
     const teams = await this.teamModel.findAll();
-    const matches = await this.matchModel.findAll();
+    const matches = await this.matchModel.findAll(false);
+    const leaderboardHome: ITeamStats[] = [];
 
     teams.forEach(({ id, teamName }) => {
       const statsTeam = getTeamStats(teamName, id, matches);
-      this._leaderboardHome.push(statsTeam);
+      leaderboardHome.push(statsTeam);
     });
 
-    return { statusCode: 200, data: this._leaderboardHome };
+    const sortedLeaderboard = sortLeaderboard(leaderboardHome);
+
+    return { statusCode: 200, data: sortedLeaderboard };
   }
 }
