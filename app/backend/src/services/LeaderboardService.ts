241167e133ac5d025bd0d6b113bb955759d65084
cunhavcs <cunha.vcs@gmail.com>
2023-08-18 15:06:02 -0300

    req 29: cria LeaderboardService.leaderboard()

--- app/backend/src/services/LeaderboardService.ts
+++ app/backend/src/services/LeaderboardService.ts
@@ -4,6 +4,7 @@ import MatchModel from '../models/MatchModel';
 import { ServiceResponse } from '../Interfaces/ServiceResponse';
 import { ITeamStats } from '../Interfaces/teams/ITeamStats';
 
+import { getTeamStats } from '../utils/teamStats.util';
 import { getHomeTeamStats } from '../utils/homeTeamStats.util';
 import { getAwayTeamStats } from '../utils/awayTeamStats.util';
 import sortLeaderboard from '../utils/sortLeaderboard.util';
@@ -13,6 +14,21 @@ export default class LeaderboardService {
     private teamModel = new TeamModel(),
     private matchModel = new MatchModel(),
   ) {}
+
+  async leaderboard(): Promise<ServiceResponse<ITeamStats[]>> {
+    const teams = await this.teamModel.findAll();
+    const matches = await this.matchModel.findAll(false);
+    const leaderboard: ITeamStats[] = [];
+
+    teams.forEach(({ id, teamName }) => {
+      const statsTeam = getTeamStats(teamName, id, matches);
+      leaderboard.push(statsTeam);
+    });
+
+    const sortedLeaderboard = sortLeaderboard(leaderboard);
+
+    return { statusCode: 200, data: sortedLeaderboard };
+  }
 
   async leaderboardHome(): Promise<ServiceResponse<ITeamStats[]>> {
     const teams = await this.teamModel.findAll();
