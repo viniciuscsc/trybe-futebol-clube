e8bd2e4836e48a76cdc332da5fe2d6a25e46fa00
cunhavcs <cunha.vcs@gmail.com>
2023-08-18 15:07:23 -0300

    req 29: cria endpoint GET /leaderboard

--- app/backend/src/routes/leaderboard.route.ts
+++ app/backend/src/routes/leaderboard.route.ts
@@ -6,6 +6,11 @@ const leaderboardController = new LeaderboardController();
 const router = Router();
 
 router.get(
+  '/',
+  (req: Request, res: Response) => leaderboardController.leaderboardAway(req, res),
+);
+
+router.get(
   '/home',
   (req: Request, res: Response) => leaderboardController.leaderboardHome(req, res),
 );
